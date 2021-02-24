a42f06dd609225bb5e0c844d0c42ccc6ab7bd8ef
Carlos <cemargato@gmail.com>
2021-02-24 19:13:38 -0300

    introducao-a-javascript-exercicioDeFixacao-1

---
+++ javascript/introducao-a-javascript-e-logica-de-programacao_1/1.1/exerciseFixative-1-Part-IV.js
@@ -0,0 +1,12 @@
+//If - Else If - Else
+
+let notaCandidato = 94;
+
+if (notaCandidato >= 80) {
+  console.log("Parabéns, você foi aprovado!")
+
+} else if (notaCandidato < 80 && notaCandidato >= 60) {
+  console.log("Você está na nossa lista de espera!")
+} else {
+  console.log("Você foi reprovado!");
+}
