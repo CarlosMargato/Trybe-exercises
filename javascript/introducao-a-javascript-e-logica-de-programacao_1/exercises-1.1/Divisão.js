472ed5891b78bfc3a54ed20b4cef001812055f9c
Carlos <cemargato@gmail.com>
2021-02-24 23:45:05 -0300

    introducao-a-javascript-exercises-1.1-part-I

---
+++ javascript/introducao-a-javascript-e-logica-de-programacao_1/exercises-1.1/Divisão.js
@@ -0,0 +1,4 @@
+let a = 24;
+let b = 4;
+
+console.log(a/b);
---
+++ javascript/introducao-a-javascript-e-logica-de-programacao_1/exercises-1.1/TureFalseImpar.js
@@ -0,0 +1,10 @@
+let valor = 34;
+let nota = 33;
+let ponto = 34;
+
+if (valor%2==1 || nota%2==1 || ponto % 2 == 1) {
+
+  console.log("True");
+} else {
+  console.log("False");
+}
