d60168e518b4ac4e35d09372c7d2149e8c32e2d8
Carlos <cemargato@gmail.com>
2021-02-27 16:55:30 -0300

    exercises 1.2(4.1)-partII

---
+++ javascript/introducao-a-javascript-e-logica-de-programacao_1/exercises1.2/array1a25impar.js
@@ -0,0 +1,9 @@
+let vintecinco = [];
+let totalquantity = 25;
+
+for (let index = 0; index <= totalquantity; index += 1) {
+  if(index%2==1){
+    vintecinco.push(index)
+  }
+}
+console.log(vintecinco);
